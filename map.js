19580225242d68729656599050269d109d328b24
David Kim <davidjinuk90@gmail.com>
2017-01-13 00:34:32 +0000

    Changed the function according to the question

--- map.js
+++ map.js
@@ -1,6 +1,27 @@
+// var words = ["ground", "control", "to", "major", "tom"];
+
+// var wordsLength = words.map(function(word) {
+//   return word.length;
+// });
+// console.log(wordsLength);
+
+
+// var words = ["ground", "control", "to", "major", "tom"];
+
+
+
 var words = ["ground", "control", "to", "major", "tom"];
 
-var wordsLength = words.map(function(word) {
+function map (arr, cb) {
+  var wordsArray = [];
+  for(var i = 0; i < arr.length; i++) {
+    wordsArray[i] = cb(arr[i]);
+  }
+  return wordsArray;
+}
+
+
+var result = map(words, function(word) {
   return word.length;
 });
-console.log(wordsLength);
+console.log(result);
